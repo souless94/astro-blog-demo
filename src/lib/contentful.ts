8938187d557e2481a86137f423f0b74608a7f98d
souless94 <clwk94@gmail.com>
2023-10-26 22:41:08 +0800

    completed blog
    Signed-off-by: souless94 <clwk94@gmail.com>

---
+++ src/lib/contentful.ts
@@ -0,0 +1,20 @@
+import contentful, { type EntryFieldTypes } from "contentful";
+
+export interface BlogPost {
+  contentTypeId: "blogPost",
+  fields: {
+    title: EntryFieldTypes.Text,
+    description: EntryFieldTypes.Text,
+    date: EntryFieldTypes.Date,
+    slug: EntryFieldTypes.Text
+    content: EntryFieldTypes.RichText,
+  }
+}
+
+export const contentfulClient = contentful.createClient({
+  space: import.meta.env.CONTENTFUL_SPACE_ID,
+  accessToken: import.meta.env.DEV
+    ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
+    : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
+});
